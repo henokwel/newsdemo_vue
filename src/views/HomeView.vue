7437534e9fdee92eb5091c82e62a4223286cda39
henokwel <henokwel@outlook.com>
2022-02-24 00:50:19 +0100

    added fallback data in case api reach limit of fail

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
 import DailyHeadline from "@/components/DailyHeadline.vue";
 import OpinionSection from "@/components/DailyOpinion.vue";
 import QuickRead from "@/components/DailyQuickRead.vue";
-
+import NewData from "../../data.json";
 export default {
   name: "HomeView",
   components: {
@@ -29,17 +29,27 @@ export default {
   created() {
     this.getData();
   },
+
   methods: {
     async getData() {
-      const res = await fetch(
-        `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`
-      );
-      const data = await res.json();
- 
-      if (data.status === "ok") {
+      try {
+        const res = await fetch(
+          `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${process.env.VUE_APP_NEWS_API_KEY}`
+        );
+        const data = await res.json();
+        console.log(data);
+
+        console.log("NewData", NewData);
+
         this.headlineArticles = await data.articles;
         this.opinionArticles = await data.articles;
         this.quickReadArticles = await data.articles;
+      } catch (error) {
+        this.headlineArticles = NewData;
+        this.opinionArticles = NewData;
+        this.quickReadArticles = NewData;
+
+        console.log("Static Data in use", error);
       }
       // console.log("headline", this.headlineArticles);
       // console.log("opinion", this.opinionArticles);
